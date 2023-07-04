16a982f8c517aa4274f5b802c09e461dab5259b4
vanessanyc <elm.vanessa.louis@gmail.com>
2023-07-04 15:45:51 -0400

    private

--- visualizer.py
+++ visualizer.py
@@ -1,13 +1,9 @@
 import spotipy as sp
 import matplotlib.pyplot as plt
 import numpy as np
+from dotenv import load_dotenv
+import os
 
-# make data with uneven sampling in x
-x = [-3, -2, -1.6, -1.2, -.8, -.5, -.2, .1, .3, .5, .8, 1.1, 1.5, 1.9, 2.3, 3]
-X, Y = np.meshgrid(x, np.linspace(-3, 3, 128))
-Z = (1 - X/2 + X**5 + Y**3) * np.exp(-X**2 - Y**2)
-
-# plot
-fig, ax = plt.subplots()
-
-ax.pcolormesh(X, Y, Z, vmin=-0.5, vmax=1.0)
+load_dotenv()
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
